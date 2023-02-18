6d3f58b8feed4bf372679ff224ae77568b4567a2
kon3gor <kon3gor@gmail.com>
2023-02-18 08:33:50 +0300

    Changed notes dir

--- internal/handlers/note/command.go
+++ internal/handlers/note/command.go
@@ -12,7 +12,6 @@ import (
 const Cmd = "note"
 const titleLen = 12
 
-// todo: save notes somewhere else
 func ProcessCommand(context *context.Context) {
 	if err := context.Guard(guard.DefaultUserNameGuard); err != nil {
 		context.TextAnswer(err.Msg)
@@ -29,7 +28,7 @@ func guarded(context *context.Context) {
 		titleLen = len(note)
 	}
 	title := note[:titleLen-1]
-	path := fmt.Sprintf("notes/%s.md", title)
+	path := fmt.Sprintf("notes/junk/%s.md", title)
 	pushReq := ghclient.NewMyPushRequest("PersonalObsidian", "main", path, note)
 	client := &http.Client{}
 	if err := ghclient.PushContent(client, pushReq); err != nil {
