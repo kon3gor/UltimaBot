53c5acc58abd2ce755dc0f478ec61d359cbeea72
kon3gor <kon3gor@gmail.com>
2023-02-18 08:30:55 +0300

    Added reminders list

--- internal/handlers/reminder/command.go
+++ internal/handlers/reminder/command.go
@@ -3,9 +3,13 @@ package reminder
 import (
 	"dev/kon3gor/ultima/internal/context"
 	"dev/kon3gor/ultima/internal/guard"
+	"fmt"
+	"math/rand"
 	"strconv"
 	"strings"
 	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 const Cmd = "remind"
@@ -30,17 +34,37 @@ func guarded(context *context.Context) {
 func processSubCommand(context *context.Context, subCommand string, args []string) {
 	switch subCommand {
 	case "list":
-		context.TextAnswer("Not yet available")
+		if len(reminders) > 0 {
+			msg := tgbotapi.NewMessage(context.ChatID, "Currently running reminders")
+			msg.ReplyMarkup = listReminders()
+			context.CustomAnswer(msg)
+		} else {
+			context.TextAnswer("No reminders running")
+		}
 	case "new":
 		createNewTimer(context, args)
 	}
 
 }
 
+var reminders map[int]string = make(map[int]string, 0)
+
+func listReminders() tgbotapi.InlineKeyboardMarkup {
+	var rows [][]tgbotapi.InlineKeyboardButton
+	for id, v := range reminders {
+		data := fmt.Sprintf("remind:%d", id)
+		button := tgbotapi.NewInlineKeyboardButtonData(v, data)
+		rows = append(rows, []tgbotapi.InlineKeyboardButton{button})
+	}
+	return tgbotapi.InlineKeyboardMarkup{InlineKeyboard: rows}
+}
+
 func createNewTimer(context *context.Context, args []string) {
+	id := rand.Intn(1000)
 	parts := strings.SplitN(args[1], " ", 2)
 	duration := timeFromString(parts[0])
-	go ticker(context, parts[1], duration)
+	reminders[id] = parts[1][:10]
+	go ticker(context, parts[1], duration, id)
 }
 
 func timeFromString(strTime string) time.Duration {
@@ -62,10 +86,11 @@ func timeFromString(strTime string) time.Duration {
 	return time.Duration(0)
 }
 
-func ticker(context *context.Context, textToSpam string, dur time.Duration) {
+func ticker(context *context.Context, textToSpam string, dur time.Duration, id int) {
 	timer := time.NewTimer(dur)
 	<-timer.C
 	context.TextAnswer(textToSpam)
+	delete(reminders, id)
 }
 
 func ProcessFlow(context *context.Context) {
